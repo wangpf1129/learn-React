c1d7480efa0d2bae1808e882a3453827aa1894da
Wangpf <1204981117@qq.com>
2021-01-28 22:28:18 +0800

    使用antd库， 并按需引入样式， 并配置自定义主题

---
+++ 7.src_路由/App.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import { Route, Switch, Redirect } from 'react-router-dom'
+
+import About from './pages/About'
+import Home from './pages/Home'
+
+
+import MyNavLink from './components/MyNavLink'
+import Header from './components/Header'
+
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            <div className="page-header">
+              <Header />
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
+              <MyNavLink to="/home">Home</MyNavLink>
+              <MyNavLink to="/about">About</MyNavLink>
+            </div>
+          </div>
+          <div className="col-xs-6">
+            <div className="panel">
+              <div className="panel-body">
+                <Switch>
+                  <Route path="/home" component={Home} />
+                  <Route path="/about" component={About} />
+                  <Redirect to="/home" />
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,19 @@
 import React, { Component } from 'react'
-import { Route, Switch, Redirect } from 'react-router-dom'
 
-import About from './pages/About'
-import Home from './pages/Home'
-
-
-import MyNavLink from './components/MyNavLink'
-import Header from './components/Header'
+import { Button, Slider } from 'antd';
 
 
 export class App extends Component {
+
+  state = {
+    disabled: false,
+  };
   render() {
+    const { disabled } = this.state;
     return (
       <div>
-        <div className="row">
-          <div className="col-xs-offset-2 col-xs-8">
-            <div className="page-header">
-              <Header />
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-xs-2 col-xs-offset-2">
-            <div className="list-group">
-              <MyNavLink to="/home">Home</MyNavLink>
-              <MyNavLink to="/about">About</MyNavLink>
-            </div>
-          </div>
-          <div className="col-xs-6">
-            <div className="panel">
-              <div className="panel-body">
-                <Switch>
-                  <Route path="/home" component={Home} />
-                  <Route path="/about" component={About} />
-                  <Redirect to="/home" />
-                </Switch>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Button type="primary">Primary Button</Button>
+        <Slider defaultValue={30} disabled={disabled} />
       </div>
     )
   }
